5c02d4c377c5248cd62ff29644d447a603051233
corynsexton <coryn.sexton@gmail.com>
2023-01-18 12:04:53 +0000

    Updates manual.py

--- RPS-Template.py
+++ RPS-Template.py
@@ -1,27 +1,28 @@
-import cv2
-from keras.models import load_model
+import cv2                                                      # Library in python to access camera
+from keras.models import load_model                             # Keras makes easier to use tensorflow
 import numpy as np
-model = load_model('keras_model.h5')
-cap = cv2.VideoCapture(0)
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+model = load_model('keras_model.h5')                            # Loads model created on Teaching-Machine
+cap = cv2.VideoCapture(0)                                       # Assigns first camera available to 'cap' - not capturing any image here yet
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)     # Creates an empty array/matrix with 4 dimensions. 1 image passed in, H=224, W=224, 3 number of channels (RGB)
 list_options = ['Rock', 'Paper', 'Scissors', 'Nothing']
 
 while True: 
-    ret, frame = cap.read()
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image
-    data[0] = normalized_image
-    prediction = model.predict(data)
-    cv2.imshow('frame', frame)
-    # Press q to close the window
+    ret, frame = cap.read()                                     # Takes first capture from line 5. Ret = Boolean (whether we could get an image or not (maybe no camera))
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA) # Re-sizes image that model is asking for
+    image_np = np.array(resized_frame)                          # Turns image into a numpy array. Changes data
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 # Normalize the image. 
+    data[0] = normalized_image                                  # Feeds normalized image into `cap` ready to pass to model.
+    prediction = model.predict(data)                            # Pass data to model to make a prediction. (This is what we can see in each line in terminal)
+    cv2.imshow('frame', frame) 
+                                                                
     print(prediction)
-    max_index = np.argmax(prediction[0])
-    print(list_options[max_index])
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    max_index = np.argmax(prediction[0])                        # Gives us the highest probability of action 
+    print(list_options[max_index])                              # Gives the corresponding element to the index in the list
+    if cv2.waitKey(1) & 0xFF == ord('q'):                       # Press q to close the window
         break
             
-# After the loop release the cap object
-cap.release()
-# Destroy all the windows
-cv2.destroyAllWindows()
+cap.release()                                                   # After the loop release the cap object
+cv2.destroyAllWindows()                                         # Destroy all the windows
+
+
--- manual_rps.py
+++ manual_rps.py
@@ -1,45 +1,42 @@
-import random                                                                       # Import random module                                               
-
-rps_choices = ["rock", "paper", "scissors"]                                         # List for computer to select choice from
+import random                                                                           # Import random module                                               
              
-def get_computer_choice():                                                          # Creates a function to generate a computer choice for game
-    computer_choice = random.choice(rps_choices)                                    # Using random module to select a random choice from list
-    print(f"Comp choice: {computer_choice}")                                        # Prints computer's coice
-    return computer_choice                                                          # Returns computer's choice from function
+def get_computer_choice():                                                              # Creates a function to generate a computer choice for game
+    computer_choice = random.choice(["rock", "paper", "scissors"])                      # Using random module to select a random choice from list                                      
+    print(f"Comp choice: {computer_choice}")                                            # Prints computer's choice
+    return computer_choice                                                              # Returns computer's choice from function                                                  
     
 
-def get_user_choice():                                                              # Creates a function to generate user's choice                        
-    user_choice = input("Rock, Paper, Scissors? ").lower()                          # Asks user to enter a choice and uses lower method to make all lower case 
+def get_user_choice():                                                                  # Creates a function to generate user's choice  
+    while True:
+        user_choice = input("Rock, Paper, Scissors? ").lower()                          # Asks user to enter a choice and uses lower method to make all lower case                                                                             
+        if user_choice in ["rock", "paper", "scissors"]:                                # Ensures user's choice is in list of options
+            print(f"User guess: {user_choice}")                                         # Prints user's choice
+            return user_choice                                                          # Returns user's chioice if valid guess
+        else:
+            print("Invalid input. Please choose Rock, Paper or Scissors. Try again!")   # Tells user choice is invalid and to try again
 
-    if user_choice not in rps_choices:                                              # Ensures user's choice is in our list of options
-        print("Invalid input. Please choose Rock, Paper or Scissors. Try again!")   # Tells user choice is invalid and to try again
-        get_user_choice()
-    else:
-        print(f"User guess: {user_choice}")                                         # Prints user's choice
-        return user_choice                                                          # Returns user's choice from function
 
+def get_winner(computer_choice, user_choice):                                           # Creates a function to determine who wins                  
 
-def get_winner(computer_choice, user_choice):                                       # Creates a function to determine who wins                  
-
-    if computer_choice == user_choice:                                              # Ensures same choices will result in a tie
+    if computer_choice == user_choice:                                                  # Ensures same choices will result in a tie
         print(f"It's a tie!")
 
-    elif computer_choice == 'rock' and user_choice == 'scissors':                   # Ensures Rock beats Scissors                  
+    elif computer_choice == 'rock' and user_choice == 'scissors':                       # Ensures Rock beats Scissors                  
         print(f"You lose. {computer_choice} beats {user_choice}")
 
-    elif computer_choice == 'paper' and user_choice == 'rock':                      # Ensures Paper beats Rock
+    elif computer_choice == 'paper' and user_choice == 'rock':                          # Ensures Paper beats Rock
         print(f"You lose. {computer_choice} beats {user_choice}")
 
-    elif computer_choice == 'scissors' and user_choice == 'paper':                  # Ensures Scissors beats Paper
+    elif computer_choice == 'scissors' and user_choice == 'paper':                      # Ensures Scissors beats Paper
         print(f"You lose. {computer_choice} beats {user_choice}")
 
     else:
-        print(f"You win! {user_choice} beats {computer_choice}")                    # Ensures user wins if computer does not
+        print(f"You win! {user_choice} beats {computer_choice}")                        # Ensures user wins if computer does not
 
 
-def play():                                                                         # Creates function to allow game to play
+def play():                                                                             # Creates function to allow game to play
     user_choice = get_user_choice()                                                 
     computer_choice = get_computer_choice()                                                                                                             
-    get_winner(computer_choice, user_choice)                                        # Calls get_winner function with computer choice and user choice passed in
+    get_winner(computer_choice, user_choice)                                            # Calls get_winner function with computer choice and user choice passed in
  
 play()
